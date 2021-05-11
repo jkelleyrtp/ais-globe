3cd80128c7b9b52cec7197df0f80b2d753acb5f7
Jonathan Kelley <jkelleyrtp@gmail.com>
2021-05-11 12:17:28 -0400

    Feat: update bundle

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import Globe from "react-globe.gl";
 let data: number[][] = [];
 
 async function main() {
-  const prom = fetch("http://localhost:3000/aispoints.json")
+  const prom = fetch("ais-globe/aispoints.json")
     .then((r) => r.json())
     .then((d) => {
       data = d;
